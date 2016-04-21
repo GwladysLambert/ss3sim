b7e23f0b216be07e107bff6134b49f06d02c392c
unknown <gwladys.lambert@AKC2SL1116-GL11.nmfs.local>
2016-04-21 16:51:32 -0700

    add line to correct agecomp Nsamp from calcomp

--- R/clean_data.r
+++ R/clean_data.r
@@ -163,6 +163,7 @@ clean_data <- function(dat_list, index_params=NULL, lcomp_params=NULL,
           lapply(1:length(calcomp_params$fleets), function(i)
              calcomp[calcomp$FltSvy == calcomp_params$fleets[i] &
                          calcomp$Yr %in% calcomp_params$years[[i]],]))
+          new.agecomp$Nsamp <- aggregate(Nsamp ~ Yr,new.calcomp, sum)
           # 
       }
     ## Create clean dat file
