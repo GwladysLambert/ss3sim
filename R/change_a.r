adf588bdab80853ce0bf8d08131247860c9334ec
unknown <gwladys.lambert@AKC2SL1116-GL11.nmfs.local>
2016-04-28 12:28:18 -0700

    changed syntax of the function

--- R/change_a.r
+++ R/change_a.r
@@ -1,108 +1,71 @@
-change_a <- function(dat_list, outfile, fleets, seas=NULL, gender=NULL, years, agerr_mat, which_agerr, by.fleets=NULL, 
-                     by.seas=NULL, by.gender=NULL, by.years=NULL, write_file=TRUE){
+change_a <- function(dat_list, outfile, df, agerr_mat, write_file=TRUE){
   
   # remember that the default will be ageing error matrix 1 - no ageing error
   
   agecomp <- dat_list$agecomp
+  df <- as.data.frame(df)
   
   # replace the number of defined matrices and the matrices in the .dat file
-  new_mat <- as.data.frame(do.call(rbind,agerr_mat))
-  names(new_mat) <- paste("age",0:c(ncol(new_mat)-1), sep="")
-  dat_list$ageerror <- dat_list$ageerror[,c(1:dim(new_mat)[2])]# ageing error matrices must have same dimensions
-  dat_list$ageerror <- rbind(dat_list$ageerror,new_mat)
-
+  new_mat <- as.data.frame(agerr_mat)
+  names(new_mat) <- c("which_matrix",paste("age",0:c(ncol(new_mat)-2), sep=""))
+  dat_list$ageerror <- dat_list$ageerror[,c(1:c(dim(new_mat)[2]-1))]# ageing error matrices must have same dimensions
+  dat_list$ageerror <- rbind(dat_list$ageerror,new_mat[,-1])
+  
   dat_list$N_ageerror_definitions <- dim(dat_list$ageerror)[1]/2
   
-    # argument which_agerr must be a list organised as follow: 
-  # one value to use only one matrix for all age data
-  # or vector of appropriate length
+  # Now replace Agerr in agecomp from df data
   
-  # if only one matrice used for all year, season, fleet and gender 
-  if (length(which_agerr)==1) agecomp$Ageerr <- which_agerr[[1]]
-
-  # if a different ageing error matrix for each fleet
-  if (!is.null(by.fleets) & is.null(by.seas) & is.null(by.gender) & is.null(by.years)) { # Length of which_agerr must be equal to length of fleets
-    for ( ii in 1:length(fleets)){
-      agecomp[agecomp$FltSvy==fleets[ii], "Ageerr"] <- which_agerr[[ii]]
-    }
-  } 
-  
-  # if a different ageing error matrix every year
-  if (is.null(by.fleets) & is.null(by.seas) & is.null(by.gender) & !is.null(by.years)) { # Length of which_agerr must be equal to length of year
-      for ( ii in 1:length(years)){
-              agecomp[agecomp$Yr==years[ii], "Ageerr"] <- which_agerr[[ii]]
+  for (yr in unique(agecomp$Yr)){
+    sub <- agecomp[agecomp$Yr==yr,]
+    for(seas in unique(sub$Seas)){
+      sub1 <- sub[sub$Seas==seas,]
+      if (dim(sub1)[1]>0){
+        for(flt in unique(sub1$FltSvy)){
+          sub2 <- sub1[sub1$FltSvy==flt,]
+          if (dim(sub2)[1]>0){
+            for(sex in unique(sub2$Gender)){
+              agecomp[agecomp$Yr==yr & agecomp$Seas==seas & agecomp$FltSvy==flt & agecomp$Gender==sex,"Ageerr"] <-
+                df[df$Yr==yr & df$Seas==seas & df$FltSvy==flt & df$Gender==sex,"Ageerr"]
             }
           }
-        
-  # if a different ageing error matrix for each season
-  if (is.null(by.fleets) & !is.null(by.seas) & is.null(by.gender) & is.null(by.years)) { 
-    for ( ii in 1:length(seas)){
-      agecomp[agecomp$Seas==seas[ii], "Ageerr"] <- which_agerr[[ii]]
+        }
+      }
     }
   }
   
-  # if a different ageing error matrix for each gender
-  if (is.null(by.fleets) & is.null(by.seas) & !is.null(by.gender) & is.null(by.years)) { 
-    for ( ii in 1:length(gender)){
-      agecomp[agecomp$Gender==gender[ii], "Ageerr"] <- which_agerr[[ii]]
-    }
-  }
-  
-  
-  ### COMBINATIONS
-  
-  ## Combination of ageing error per fleet and per year - which_agerr should be organised as fleet 1 first all years, then fleet 2 all years etc
-  if (!is.null(by.fleets) & is.null(by.seas) & is.null(by.gender) & !is.null(by.years)) { 
-    for (ii in 1:length(fleets)) {
-    for ( jj in 1:length(years)){
-      agecomp[agecomp$FltSvy==fleets[ii] & agecomp$Yr==years[jj], "Ageerr"] <- which_agerr[[i]][[jj]]
-    }
-    }
-  }
-  
-  ## Combination of ageing error per season and per year - which_agerr should be organised as season 1 first all years, then season 2 all years etc
-  if (is.null(by.fleets) & !is.null(by.seas) & is.null(by.gender) & is.null(by.years)) { 
-    for (ii in 1:length(seas)) {
-      for ( jj in 1:length(years)){
-        agecomp[agecomp$Seas==seas[ii] & agecomp$Yr==years[jj], "Ageerr"] <- which_agerr[[i]][[jj]]
-      }
-  }
-  }
-    
-  ## Combination of ageing error per gender and per year - which_agerr should be organised as gender 1 first all years, then gender 2 all years etc
-  if (is.null(by.fleets) & is.null(by.seas) & !is.null(by.gender) & !is.null(by.years)) { 
-    for (ii in 1:length(gender)) {
-      for ( jj in 1:length(years)){
-        agecomp[agecomp$Gender==gender[ii] & agecomp$Yr==years[jj], "Ageerr"] <- which_agerr[[i]][[jj]]
-      }
-  }
-  }
-    
-  ## Combination of ageing error per fleet and per season - which_agerr should be organised as fleet 1 first all seasons, then fleet 2 all seasons etc
-  if (!is.null(by.fleets) & !is.null(by.seas) & is.null(by.gender) & is.null(by.years)) { # Length of which_agerr must be equal to length of year
-    for (ii in 1:length(fleets)){
-      for ( jj in 1:length(seas)){
-        agecomp[agecomp$FltSvy==fleets[ii] & agecomp$Seas==seas[jj], "Ageerr"] <- which_agerr[[i]][[jj]]
-      }
-  }
-  }
-    
-  ## Combination of ageing error per fleet and per gender - which_agerr should be organised as fleet 1 first all genders, then fleet 2 all genders etc
-  if (!is.null(by.fleets) & is.null(by.seas) & !is.null(by.gender) & is.null(by.years)) { # Length of which_agerr must be equal to length of year
-    for (ii in 1:length(fleets)){
-      for ( jj in 1:length(gender)){
-        agecomp[agecomp$FltSvy==fleets[ii] & agecomp$Gender==gender[jj], "Ageerr"] <- which_agerr[[i]][[jj]]
-      }
-  }
-  }
-    
   agecomp -> dat_list$agecomp
   
-## Write the modified file
+  # If there are mean length at age they need to be replaced too
+  
+  if (dat_list$N_MeanSize_at_Age_obs !=0) {
+    
+    mlacomp <- dat_list$MeanSize_at_Age_obs
+    
+    for (yr in unique(mlacomp$Yr)){
+      sub <- mlacomp[mlacomp$Yr==yr,]
+      for(seas in unique(sub$Seas)){
+        sub1 <- sub[sub$Seas==seas,]
+        if (dim(sub1)[1]>0){
+          for(flt in unique(sub1$FltSvy)){
+            sub2 <- sub1[sub1$FltSvy==flt,]
+            if (dim(sub2)[1]>0){
+              for(sex in unique(sub2$Gender)){
+                mlacomp[mlacomp$Yr==yr & mlacomp$Seas==seas & mlacomp$FltSvy==flt & mlacomp$Gender==sex,"Ageerr"] <-
+                  df[df$Yr==yr & df$Seas==seas & df$FltSvy==flt & df$Gender==sex,"Ageerr"]
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    mlacomp -> dat_list$MeanSize_at_Age_obs
+    
+  }
+  
+  ## Write the modified file
   if(write_file) {
     SS_writedat(datlist = dat_list, outfile = outfile, overwrite = TRUE, verbose = FALSE)
-  invisible(dat_list)
+    invisible(dat_list)
   }
 }
-
-
